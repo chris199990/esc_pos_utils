c86dbb743c313fd4fa602021c6099f3030c92643
chris199990 <160700291+chris199990@users.noreply.github.com>
2024-05-10 17:18:01 +0800

    Update generator.dart

--- lib/src/generator.dart
+++ lib/src/generator.dart
@@ -66,7 +66,7 @@ class Generator {
     return charsPerLine;
   }
 
-  Uint8List _encode(String text, {bool isKanji = false}) {
+  Uint8List _encode(String text, {bool isKanji = false, String? cTable}) {
     // replace some non-ascii characters
     text = text
         .replaceAll("’", "'")
@@ -349,7 +349,7 @@ class Generator {
     int linesAfter = 0,
     bool containsChinese = false,
     int? maxCharsPerLine,
-    String cTable,
+    String? cTable,
   }) {
     List<int> bytes = [];
     if (!containsChinese) {
